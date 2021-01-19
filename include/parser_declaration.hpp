f26e03e038837bc8b4ebf26679ae4552c23aeaf5
n-arms <75282394+n-arms@users.noreply.github.com>
2021-01-19 16:31:19 -0500

    Add declarations for Parsing * Define the ABC Line to rep a parsed line of simple * Declare the 3 subclasses: Declaration, Definition and Call

--- include/parser_declaration.hpp
+++ include/parser_declaration.hpp
@@ -17,13 +17,62 @@ namespace simple{
   class BinOp;
   class Variable;
 
-  union Value;
+  union VarValue;
 
   enum CallType {gotoCall, tagCall, returnCall, forCall, ifCall, writeCall, readCall} ;
   enum VarType {intVar, realVar, charVar, intArr, realArr, charArr} ;
+  enum LineType {lineCall, definitionCall, declarationCall} ;
+
+  class Line{
+  public:
+    virtual void print() const = 0;
+    virtual const LineType& lineType() const = 0;
+  } ;
+
+  class Declaration : public Line{
+  protected:
+    std::string name_;
+    VarType type_;
+    unsigned long long length_;
+  public:
+    Declaration(VarType var, std::string name, unsigned long long length = 1);
+    ~Declaration();
+    const LineType& lineType() const override;
+    void print() const override;
+    const std::string& name() const;
+    const VarType& type() const;
+    const unsigned long long& length() const;
+
+  } ;
+
+  class Definition : public Line{
+  protected:
+    std::vector<Expression> value_;
+    std::string name_;
+  public:
+    Definition(std::vector<Expression> value, std::string name);
+    ~Definition();
+    const LineType& lineType() const override;
+    void print() const override;
+    const std::string& name() const;
+    const std::vector<Expression>& value() const;
+    const unsigned long long length() const;
+  } ;
+
+  class Call : public Line{
+  protected:
+    CallType type_;
+    std::vector<Expression> arguments_;
+  public:
+    Call(CallType type, std::vector<Expression> arguments);
+    ~Call();
+    const LineType& lineType() const override;
+    void print() const override;
+    const CallType& type() const;
+    const std::vector<Expression>& arguments() const;
+  } ;
 
   std::vector<Line> parse(std::deque<Token*> tokens);
-
 }
 
 #endif
